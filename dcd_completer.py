b992676167da0d16bbdf3fd9ea79c5aaeb01c0ab
drpriver <david@davidpriver.com>
2023-09-29 20:47:20 -0700

    fix dcd_completer

--- dcd_completer.py
+++ dcd_completer.py
@@ -21,27 +21,26 @@ IncludeSymbolFilename = True
 
 _logger = logging.getLogger(__name__)
 
-def log(level, msg):
-    _logger.log(level, '[dcdcompl] %s' % msg)
+def log(level, msg, *args):
+    _logger.log(level, '[dcdcompl] '+ msg, *args)
 
-def error(msg):
-    log(logging.ERROR, msg)
+def error(msg, *args):
+    log(logging.ERROR, msg, *args)
 
-def warning(msg):
-    log(logging.WARNING, msg)
+def warning(msg, *args):
+    log(logging.WARNING, msg, *args)
 
-def info(msg):
-    log(logging.INFO, msg)
+def info(msg, *args):
+    log(logging.INFO, msg, *args)
 
-def debug(msg):
-    log(logging.DEBUG, msg)
+def debug(msg, *args):
+    log(logging.DEBUG, msg, *args)
 
 class DCDCompleter(Completer):
     newline_re = re.compile(r'([^\\])\\n')
 
     def __init__(self, user_options):
-        super(DCDCompleter, self).__init__(user_options)
-        self.user_options = user_options
+        super().__init__(user_options)
         self._popener = utils.SafePopen
         self._binary = utils.PathToFirstExistingExecutable(['dcd-client'])
 
@@ -52,32 +51,35 @@ class DCDCompleter(Completer):
 
         info('DCD completer loaded')
 
+    # override
     def SupportedFiletypes(self):
-        return set(['d'])
+        return {'d'}
 
+    # override
     def ShouldUseNowInner(self, request_data):
-        shouldUseNowInner = len(self.ComputeCandidates(request_data)) > 0
-        return shouldUseNowInner
+        return len(self.ComputeCandidates(request_data)) > 0
 
+    # override
     def ComputeCandidates(self, request_data):
         filepath = request_data['filepath']
         linenum = request_data['line_num']
         colnum = request_data['column_num']
         contents = request_data['file_data'][filepath]['contents']
         try:
-            return [sug for sug in self._Suggest(filepath, linenum, colnum, contents) if sug]
+            return [sug for sug in self._suggest(filepath, linenum, colnum, contents) if sug]
         except:
             error(traceback.format_exc())
             return []
 
-    def _EmptyQueue(self):
-        while self._dataqueue.not_empty:
-            try:
-                self._dataqueue.get_nowait()
-            except:
-                break
+    # override
+    def GetSubcommandsMap(self):
+        return {
+            'GoTo': self.__class__._goto,
+            'GoToDefinition': self.__class__._goto,
+            'GoToDeclaration': self.__class__._goto,
+        }
 
-    def _Suggest(self, filename, linenum, column, contents):
+    def _suggest(self, filename, linenum, column, contents):
         if not contents:
             with open(filename, 'r') as f:
                 contents = f.read()
@@ -87,93 +89,86 @@ class DCDCompleter(Completer):
             if completionData[1]:
                 error('Completion error from dcd-client:\n' + completionData[1].decode('utf-8'))
                 return []
+            EXCLUDES = frozenset({
+                'identifiers',
+                '',
+                'stringof\tk',
+                'mangleof\tk',
+                'tupleof\tk',
+                'alignof\tk',
+                'init\tk',
+                'sizeof\tk',
+                'destroy\tF',
+                'hashOf\tF',
+                'opEquals\tF',
+                'toString\tF',
+                'toHash\tF',
+                'setSameMutex\tF',
+                'opCmp\tF',
+            })
 
-            completions = [self._CreateCompletionData(line, contents)
+            completions = [self._create_completion_data(line, contents)
                     for line in completionData[0].decode('utf-8').splitlines()
-                    if not line.strip() in ['identifiers', '']]
+                    if not line.startswith('_') and line.strip() not in EXCLUDES]
             return completions
         except KeyboardInterrupt:
             pass
         return []
 
-    def getCursorPos(self, linenum, column, contents):
+    def _get_cursor_pos(self, linenum, column, contents):
         endingsLength = linenum if contents.find('\r\n') < 0 else linenum * 2
         return len(''.join(contents.splitlines()[:linenum-1])) + endingsLength + column - 1
 
-    def _ExecClient(self, cmd, contents):
+    def _exec_client(self, cmd, contents):
         args = [self._binary] + cmd.split(' ')
         popen = self._popener(args, executable = self._binary,
                 stdin = PIPE, stdout = PIPE, stderr = PIPE)
         return popen.communicate(contents.encode('utf-8'))
 
-    def _CreateCompletionData(self, line, contents):
-        if line.find('\t') < 0:
+    def _create_completion_data(self, line, contents):
+        if '\t' not in line:
             return []
         name, kind = line.split('\t')
-
-        docText = self.getDocText(name, contents)
 
         longname = name
         if '.' in name:
             name = name.split('.')[-1]
             longname = name + ' (' + longname + ')'
 
-        if docText:
-            longname += ' (doc)'
-
         return responses.BuildCompletionData(
-                insertion_text = name,
-                menu_text = longname,
-                kind = kind,
-                detailed_info = DCDCompleter.newline_re.subn('\\1\n', docText)[0]
-                        .replace('\\\\', '\\').rstrip('\n')
-                )
+            insertion_text = name,
+            menu_text = longname,
+            kind = kind,
+            detailed_info = '',
+        )
 
-    def getImports(self, contents):
-        return '\n'.join(
-            [line for line in contents.splitlines()
-            if line.startswith('import') and line.strip().endswith(';')])
 
-    def getSymbolDef(self, symbol, contents):
-        text = contents + ';' + symbol
+    def _goto(self, request_data, args):
+        filepath = request_data['filepath']
+        linenum = request_data['line_num']
+        colnum = request_data['column_num']
+        contents = request_data['file_data'][filepath]['contents']
+        cursor = self._get_cursor_pos(linenum, colnum, contents)
+        data = self._exec_client(f'-l -c {cursor}', contents)[0]
+        d = data.decode('utf-8').strip()
+        if not d: return None
 
-        symData = self._ExecClient('-l -c %d' % (len(text.encode('utf-8')) - 1), text)
-        if symData[1]:
-            return None
-
-        symText = symData[0].strip()
-
-        if not b'\t' in symText:
-            return
-
-        symFilename, symPosition = symText.split(b'\t')
-        if os.path.exists(symFilename):
-            if IncludeSymbolFilename:
-                symbol = symFilename + b'\n'
+        gotos = []
+        for line in d.split('\n'):
+            if '\t' not in line:
+                continue
+            f, b = line.split('\t')
+            b = int(b)
+            if f == 'stdin':
+                f = filepath
             else:
-                symbol = b''
+                with open(f, 'r') as fp:
+                    contents = fp.read()
+            l = contents[0:b].count('\n')+1
+            c = ''.join(reversed(contents[0:b])).find('\n')+1
+            gotos.append(responses.BuildGoToResponse(f, l, c, ''))
+        if len(gotos) == 1:
+            return gotos[0]
+        return gotos if gotos else None
 
-            with open(symFilename, 'rb') as f:
-                f.seek(int(symPosition))
-                while f.tell() > 0 and f.read(1) != b'\n':
-                    f.seek(-2, 1)
 
-                for i, line in enumerate(f):
-                    if i == 0:
-                        line = line.lstrip(b' \t')
-                    symbol += line
-                    if b')' in line:
-                        break
-
-            return symbol.decode('utf-8')
-
-    def getDocText(self, symbol, contents):
-        text = contents + ';' + symbol
-
-        symbolDef = self.getSymbolDef(symbol, contents) or ''
-
-        docData = self._ExecClient('-d -c %d' % (len(text.encode('utf-8')) - 1), text)
-        if not docData[1]:
-            docText = docData[0].decode('utf-8').strip()
-
-        return symbolDef + (docText or '')
